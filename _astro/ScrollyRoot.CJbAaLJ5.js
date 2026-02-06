2cd2c22c5f7aea876805a75a282213df7caeaa7e
Kevin <kmembreno@unah.hn>
2026-02-05 22:00:01 -0600

    Updates

---
+++ _astro/ScrollyRoot.CJbAaLJ5.js
@@ -0,0 +1,1 @@
+import{S as p}from"./ScrollyRoot.DNFi3t_g.js";import"./jsx-runtime.D_zvdyIk.js";import"./index.BIIuLtGZ.js";export{p as default};
--- _astro/ScrollyRoot.YLT0GQeo.js
+++
@@ -1,1 +0,0 @@
-import{S as a}from"./ScrollyRoot.DWnGwiHo.js";import"./index.BIIuLtGZ.js";export{a as default};
